c344011cca5798d3bd9509507992401cba91622a
shazib-ahmed <shajibahmed420@gmail.com>
2024-07-01 19:27:32 +0600

    tawk to chat integrated

--- resources/views/layout/frontend/frontend.blade.php
+++ resources/views/layout/frontend/frontend.blade.php
@@ -35,6 +35,23 @@
         integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/65c54b6b0ff6374032cb0663/1hm5at760';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
+
     @yield('script')
 </body>
 
